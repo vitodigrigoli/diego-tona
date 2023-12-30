d665f3f54aeb7079697550a2f4517697b9f4c681
Vito Di Grigoli <73437231+vitodigrigoli@users.noreply.github.com>
2023-12-30 21:30:51 +0100

    barbajs

--- scripts.js
+++ scripts.js
@@ -1,75 +1,127 @@
-const price__coaching__monthly = document.getElementById('price__coaching__monthly')
-const price__coaching__quarterly = document.getElementById('price__coaching__quarterly')
-const price__coaching__halfyearly = document.getElementById('price__coaching__halfyearly')
+const initServices = (barba_document) => {
+	console.log('initServices')
+	const price__coaching__monthly = barba_document.querySelector('#price__coaching__monthly')
+	const price__coaching__quarterly = barba_document.querySelector('#price__coaching__quarterly')
+	const price__coaching__halfyearly = barba_document.querySelector('#price__coaching__halfyearly')
+	
+	const price__nutrition__monthly = barba_document.querySelector('#price__nutrition__monthly')
+	const price__nutrition__quarterly = barba_document.querySelector('#price__nutrition__quarterly')
+	const price__nutrition__halfyearly = barba_document.querySelector('#price__nutrition__halfyearly')
+	
+	const price__all__monthly = barba_document.querySelector('#price__all__monthly')
+	const price__all__quarterly = barba_document.querySelector('#price__all__quarterly')
+	const price__all__halfyearly = barba_document.querySelector('#price__all__halfyearly')
+	
+	const header__monthly = barba_document.querySelector('#monthly')
+	const header__quarterly = barba_document.querySelector('#quarterly')
+	const header__halfyearly = barba_document.querySelector('#half-yearly')
 
-const price__nutrition__monthly = document.getElementById('price__nutrition__monthly')
-const price__nutrition__quarterly = document.getElementById('price__nutrition__quarterly')
-const price__nutrition__halfyearly = document.getElementById('price__nutrition__halfyearly')
+	
+	const prices__monthly = [price__coaching__monthly, price__nutrition__monthly, price__all__monthly]
+	const prices__quarterly = [price__coaching__quarterly, price__nutrition__quarterly, price__all__quarterly]
+	const prices__halfyearly  = [price__coaching__halfyearly , price__nutrition__halfyearly , price__all__halfyearly ]
+	
+	const headers = [header__monthly, header__quarterly, header__halfyearly]
+	const prices = [prices__monthly, prices__quarterly , prices__halfyearly ]
+	
+	if (headers[0]){
+		headers.forEach(header => {
+			header.addEventListener('click', event => servicesHandler(header, headers, prices));
+		})
+	}
+	else{
+		console.log('NULL')
+	}
 
-const price__all__monthly = document.getElementById('price__all__monthly')
-const price__all__quarterly = document.getElementById('price__all__quarterly')
-const price__all__halfyearly = document.getElementById('price__all__halfyearly')
+}
 
-const header__monthly = document.getElementById('monthly')
-const header__quarterly = document.getElementById('quarterly')
-const header__halfyearly = document.getElementById('half-yearly')
+const servicesHandler = (header, headers, prices ) => {
+	console.log('servicesHandler')
 
-const package__coaching = document.getElementById('package__coaching')
-const package__nutrition = document.getElementById('package__nutrition')
-const package__all = document.getElementById('package__all')
+	const [header__monthly, header__quarterly, header__halfyearly] = headers;
+	const [prices__monthly, prices__quarterly, prices__halfyearly] = prices;
 
-const description__coaching = document.getElementById('description__coaching')
-const description__nutrition = document.getElementById('description__nutrition')
-const description__all = document.getElementById('description__all')
 
-const prices__monthly = [price__coaching__monthly, price__nutrition__monthly, price__all__monthly]
-const prices__quarterly = [price__coaching__quarterly, price__nutrition__quarterly, price__all__quarterly]
-const prices__halfyearly  = [price__coaching__halfyearly , price__nutrition__halfyearly , price__all__halfyearly ]
+	if(header.id == 'monthly'){
+		prices__monthly.forEach(price => price.classList.add('service__price--active'))
+		prices__quarterly.forEach(price => price.classList.remove('service__price--active'))
+		prices__halfyearly.forEach(price => price.classList.remove('service__price--active'))
 
-const headers = [header__monthly, header__quarterly, header__halfyearly]
+		header__monthly.classList.add('services__header__item--active')
+		header__quarterly.classList.remove('services__header__item--active')
+		header__halfyearly.classList.remove('services__header__item--active')
+	}
+	else if(header.id == 'quarterly'){
+		prices__monthly.forEach(price => price.classList.remove('service__price--active'))
+		prices__quarterly.forEach(price => price.classList.add('service__price--active'))
+		prices__halfyearly.forEach(price => price.classList.remove('service__price--active'))
 
-if (!headers){
-	headers.forEach(header => {
-		header.addEventListener('click', () => {
+		header__monthly.classList.remove('services__header__item--active')
+		header__quarterly.classList.add('services__header__item--active')
+		header__halfyearly.classList.remove('services__header__item--active')
+	}
+	else{
+		prices__monthly.forEach(price => price.classList.remove('service__price--active'))
+		prices__quarterly.forEach(price => price.classList.remove('service__price--active'))
+		prices__halfyearly.forEach(price => price.classList.add('service__price--active'))
 
-			if(header.id == 'monthly'){
-				prices__monthly.forEach(price => price.classList.add('service__price--active'))
-				prices__quarterly.forEach(price => price.classList.remove('service__price--active'))
-				prices__halfyearly.forEach(price => price.classList.remove('service__price--active'))
+		header__monthly.classList.remove('services__header__item--active')
+		header__quarterly.classList.remove('services__header__item--active')
+		header__halfyearly.classList.add('services__header__item--active')
+	}
+}
 
-				header__monthly.classList.add('services__header__item--active')
-				header__quarterly.classList.remove('services__header__item--active')
-				header__halfyearly.classList.remove('services__header__item--active')
-			}
-			else if(header.id == 'quarterly'){
-				prices__monthly.forEach(price => price.classList.remove('service__price--active'))
-				prices__quarterly.forEach(price => price.classList.add('service__price--active'))
-				prices__halfyearly.forEach(price => price.classList.remove('service__price--active'))
+const removeServicesEventListener = (barba_document) => {
+	console.log('removeServicesEventListener')
+	
+	const header__monthly = barba_document.querySelector('#monthly')
+	const header__quarterly = barba_document.querySelector('#quarterly')
+	const header__halfyearly = barba_document.querySelector('#half-yearly')
 
-				header__monthly.classList.remove('services__header__item--active')
-				header__quarterly.classList.add('services__header__item--active')
-				header__halfyearly.classList.remove('services__header__item--active')
-			}
-			else{
-				prices__monthly.forEach(price => price.classList.remove('service__price--active'))
-				prices__quarterly.forEach(price => price.classList.remove('service__price--active'))
-				prices__halfyearly.forEach(price => price.classList.add('service__price--active'))
+	
+	const headers = [header__monthly, header__quarterly, header__halfyearly]
+	
+	
+	if (headers[0]){
+		headers.forEach(header => {
+			header.removeEventListener('click', servicesHandler);
+		})
+	}
 
-				header__monthly.classList.remove('services__header__item--active')
-				header__quarterly.classList.remove('services__header__item--active')
-				header__halfyearly.classList.add('services__header__item--active')
-			}
-			
-		});
-	});
 }
 
 
-const faqs = document.querySelectorAll('.faq')
 
-faqs.forEach( faq => faq.addEventListener('click', () => {
+const faqsHandler = faq => {
+	console.log('faqsHandler')
+
 	faq.classList.toggle('faq--active')
-}))
+}
+
+const initFaqs = (barba_document) => {
+	
+	console.log('initFaqs')
+
+	const faqs = barba_document.querySelectorAll('.faq')
+
+	if(faqs){
+		
+		faqs.forEach( faq => faq.addEventListener('click', event => faqsHandler(faq)))
+	}
+
+}
+
+const removeFaqsEventListener = (barba_document) => {
+
+	console.log('removeFaqsEventListener')
+
+	const faqs = barba_document.querySelectorAll('.faq')
+
+	if(faqs){
+		
+		faqs.forEach( faq => faq.removeEventListener('click', faqsHandler))
+	}
+} 
 
 
 
@@ -77,11 +129,214 @@ const navbar__button = document.querySelector('.navbar__button')
 const navbar = document.querySelector('.navbar')
 
 navbar__button.addEventListener('click', () =>{
+	console.log('navbar__button')
+
 	navbar__button.classList.toggle('navbar__button--active')
 	navbar.classList.toggle('navbar--active')
 })
 
 
 
+const navbar__body__item = document.querySelectorAll('.navbar__body__item')
+
+navbar__body__item.forEach(item => item.addEventListener('click', () => {
+	navbar.classList.remove('navbar--active')
+})) 
+
+const navbar__header = document.querySelector('.navbar__header')
+navbar__header.addEventListener('click', () => navbar.classList.remove('navbar--active'))
 
 
+const buttonsHandler = () => {
+
+	console.log('buttonsHandler')
+
+	const buttons = document.querySelectorAll('.floating'); 
+	let scrollPosition = window.scrollY + window.innerHeight;
+	let totalHeight = document.body.offsetHeight;
+	
+	// Sostituisci '100' con il valore che rappresenta la distanza dal fondo pagina per attivare l'effetto
+	
+	if (totalHeight - scrollPosition < 100) { 
+		buttons.forEach(function(button) {
+		button.classList.add('floating--hidden');
+		});
+	} else {
+		buttons.forEach(function(button) {
+		button.classList.remove('floating--hidden');
+		});
+	}
+
+}
+
+const initButtons = () => {
+
+	console.log('initButtons')
+	window.addEventListener('scroll', buttonsHandler);
+}
+
+
+
+const initImageCompare = (barba_document) => {
+	console.log('initImageCompare')
+	const viewers = barba_document.querySelectorAll(".image-compare");
+
+	if(viewers){
+
+		const options = {
+		
+			// UI Theme Defaults
+		  
+			controlColor: "#FFFFFF",
+			controlShadow: true,
+			addCircle: true,
+			addCircleBlur: true,
+		  
+			// Label Defaults
+		  
+			showLabels: true,
+			labelOptions: {
+			  before: 'Before',
+			  after: 'After',
+			  onHover: false
+			},
+		  
+			// Smoothing
+		  
+			smoothing: true,
+			smoothingAmount: 100,
+		  
+			// Other options
+		  
+			hoverStart: true,
+			verticalMode: false,
+			startingPoint: 50,
+			fluidMode: true
+		};
+		  
+		viewers.forEach((element) => {
+		  new ImageCompare(element, options).mount();
+		});
+	}
+
+}
+
+
+const initVideo = (barba_document) =>{
+
+	console.log('initVideo')
+
+	let video = barba_document.querySelector('video');
+
+		if (video) {
+			video.play();
+		}
+}
+
+
+
+
+
+
+
+
+function pageTransition() {
+
+	let tl = gsap.timeline();
+
+	tl.to('.transition', {
+		duration: 1,
+		scaleY: 1,
+		transformOrigin: 'bottom',
+		ease: 'power4.inOut'
+	});
+
+	tl.to('.transition', {
+		duration: 1,
+		scaleY: 0,
+		transformOrigin: 'top',
+		ease: 'power4.inOut',
+		delay: 0.2
+	});
+
+}
+
+
+function contentAnimation() {
+
+	let tl = gsap.timeline();
+
+	tl.to('.reveal__item', {
+		scaleY: 0,
+		transformOrigin: 'top',
+		duration: 1,
+		ease: 'power3.inOut',
+		delay: 0.75
+	});
+
+}
+
+
+function delay(n) {
+
+	n = n || 0;
+
+	return new Promise((done) => {
+		setTimeout( () => {
+			done();
+		}, n)
+	});
+}
+
+
+
+barba.init({
+
+	sync: true,
+	transitions: [
+		{
+			async leave(data) {
+				console.log('leave')
+				
+				const done = this.async();
+				pageTransition();
+				await delay(1000);
+
+				removeServicesEventListener(data.current.container)
+				removeFaqsEventListener(data.current.container)
+				
+				done();
+			},
+
+			async enter(data) {
+				console.log('enter')
+
+				window.scrollTo(0, 0);
+
+				contentAnimation();
+
+				initVideo(data.next.container);
+				initServices(data.next.container)
+				initImageCompare(data.next.container)
+				initFaqs(data.next.container)
+
+			},
+
+			async once(data) {
+				console.log('once')
+
+				pageTransition();
+				await delay(1000);
+				contentAnimation();
+
+				initVideo(data.next.container);
+				initServices(data.next.container);
+				initImageCompare(data.next.container);
+				initButtons()
+				initFaqs(data.next.container)
+			},
+
+
+		},
+	],
+});
